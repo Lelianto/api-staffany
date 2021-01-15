4d121f0b44f165460d80af75bb1ad02107a28ed9
Lelianto Eko Pradana <lelianto.eko@yesdok.com>
2021-01-15 19:17:54 +0700

    make it better

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ require('dotenv').config()
 
 const app = express()
 
+const port = process.env.PORT || 9000;
+
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -28,6 +30,6 @@ mongoose.connect(`${process.env.MONGO_URI}`, {
 app.use(shiftRouter)
 
 
-app.listen(9000, () => {
+app.listen(port, () => {
     console.log('server is listening on port 9000')
 })
